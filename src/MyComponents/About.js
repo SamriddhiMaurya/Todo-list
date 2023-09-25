a34f385591816103b6b9a65dd40e488bad0367d7
SamriddhiMaurya <samriddimaurya9005@gmail.com>
2023-09-25 20:22:14 +0530

    main

---
+++ src/MyComponents/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      <p>This to-do list is mades by Samriddhi and it's a well-organized and prioritized list of tasks, 
+        reflecting her keen attention to detail and efficient planning. 
+        It likely includes a mix of personal and professional commitments, with clear deadlines and actionable steps. 
+        This to-do list helps you to stay on top of your responsibilities .
+        <br />
+        For any queries email at - <b>abc@gmail.com</b></p>
+    </div>
+  )
+}
+
+
